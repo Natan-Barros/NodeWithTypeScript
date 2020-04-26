e3c575d0da00125569560313bba09dbb66458205
Natan Barros <natanbarros133@gmail.com>
2020-04-26 16:47:45 -0300

    Concluir desafio

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,4 +1,12 @@
 import Transaction from '../models/Transaction';
+import CreateTransactionService from '../services/CreateTransactionService';
+
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+
+  type: 'income' | 'outcome';
+}
 
 interface Balance {
   income: number;
@@ -14,15 +22,37 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    if (this.transactions.length > 0) {
+      const income = this.transactions
+        .map(transaction => {
+          if (transaction.type === 'income') return transaction.value;
+          return 0;
+        })
+        .reduce((accumulator, current) => accumulator + current);
+
+      const outcome = this.transactions
+        .map(transaction => {
+          if (transaction.type === 'outcome') return transaction.value;
+          return 0;
+        })
+        .reduce((accumulator, current) => accumulator + current);
+
+      const total = income - outcome;
+      return { income, outcome, total };
+    }
+
+    return { income: 0, outcome: 0, total: 0 };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
